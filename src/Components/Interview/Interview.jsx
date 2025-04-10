9e3cb48f3625e73c2fed4d32144691fed798b7ea
Nouran-Aly <nouran3ly@gmail.com>
2025-04-10 12:11:18 +0200

    interview and prefrences update

--- src/Components/Interview/Interview.jsx
+++ src/Components/Interview/Interview.jsx
@@ -1,34 +1,139 @@
+import axios from 'axios'
+import { Field, Form, Formik, useFormik } from 'formik'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Interview() {
+    const [interviewId, setInterviewId] = useState()
+    const [questions, setQuestions] = useState()
+    const [answers, setAnswers] = useState()
+    const [timer, setTimer] = useState("")
+
+    async function getInterviewQuestions() {
+        return await axios.post(`https://intervyouquestions.runasp.net/api/Interview/start/user/${"35e53751-0c1a-4ec9-b41c-ac66df3400c1"}`,
+            {
+                params: {
+                    numberOfQuestions: 10,
+                    timeLimitInMinutes: 30
+                }
+            }
+        )
+            .then((res) => {
+                console.log(res.data);
+                setInterviewId(res.data.interviewId)
+                setTimer(res.data.timeLimitInMinutes)
+            })
+            .catch((error) => {
+                console.log(error);
+
+            })
+    }
+
+    async function showInterviewQuestions() {
+        // return await axios.get(`https://intervyouquestions.runasp.net/api/InterviewQuestions/${interviewId}/questions`)
+        return await axios.get(`https://intervyouquestions.runasp.net/api/InterviewQuestions/162/questions`)
+            .then((res) => {
+                console.log(res.data);
+                setQuestions(res.data)
+            }).catch((err) => {
+                console.log(err.response);
+            })
+    }
+
+    useEffect(() => {
+        getInterviewQuestions()
+    }, [])
+
+    useEffect(() => {
+        if (interviewId) {
+            showInterviewQuestions()
+            console.log(interviewId);
+        }
+    }, [interviewId])
+
+    useEffect(() => {
+        if (timer == 0) return;
+        const handleTimer = setInterval(() => {
+            setTimer((timer) => timer - 1)
+            return () => clearInterval(handleTimer)
+        }, 1000);
+    }, [])
+
+
+
+    // handle timer
+    // const handleTimer = setInterval(() => {
+    //     setTimer((time) => time - 1)
+    //     return () => clearInterval(handleTimer)
+    // }, timer);
+
     return (
         <div className="flex flex-col px-8 lg:px-32 py-10 gap-8 bg-[#F6F4F0] min-h-[calc(100vh-68px)]">
             <div className="flex justify-between">
                 <h1 className='text-2xl font-bold text-black'>.Net Backend Interview - Junior Level</h1>
-                <p className='font-bold text-4xl'>25:00</p>
+                <p className='font-bold text-4xl'>{timer}</p>
             </div>
             <div className="flex flex-col gap-4">
                 <p className='text-2xl font-semibold text-[#152A4C]'>Question 1/35</p>
                 <p className='text-lg text-[#696F79] italic'>Object Oriented Programming</p>
             </div>
-            <div className="question flex flex-col place-items-center gap-8 mt-5">
-                <p className='font-medium text-2xl text-center'>Which C# concept has the capability of an object to take number of different forms and hence display behaviour as accordingly?</p>
-                <ul className='mt-10 flex flex-col gap-8'>
-                    <li className='bg-white px-40 py-4.5 text-center text-2xl rounded-4xl border border-[#152A4C]'>Polymorphism</li>
-                    <li className='bg-white px-40 py-4.5 text-center text-2xl rounded-4xl border border-[#152A4C]'>Polymorphism</li>
-                    <li className='bg-white px-40 py-4.5 text-center text-2xl rounded-4xl border border-[#152A4C]'>Polymorphism</li>
-                    <li className='bg-white px-40 py-4.5 text-center text-2xl rounded-4xl border border-[#152A4C]'>Polymorphism</li>
-                </ul>
-                {/* <div className="flex items-center">
-                        <input id="link-radio" type="radio" defaultValue className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label htmlFor="link-radio" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Radio button with a <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline">link inside</a>.</label>
-                    </div> */}
-                <Link to="/Begin-Interview" className='self-end'>
-                    <button className=' bg-[#152A4C] text-white px-16 py-4 rounded-4xl font-bold'>Next</button>
-                </Link  >
-            </div>
+            <Formik
+                initialValues={{
+                    userId: "",
+                    interviewId: 0,
+                    questionId: 0,
+                    answerText: "",
+                    selectedOptionIds: []
+                }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {({ values, setFieldValue }) => (
+                    <Form >
+                        {questions?.map((question, index) => (
+                            <div key={question.questionId} className="question flex flex-col place-items-center gap-8 mt-5">
+                                <p className='font-medium text-2xl text-center'><span className='pe-2'>{index + 1}.</span>{question.text}</p>
+                                {question.type == "essay" ? (
+                                    <Field as="textarea" name="answerText" id="" className='w-full h-60 p-4 border-gray-300 rounded-lg outline-0 focus:border-[#79D7BE] border-2'></Field>
+                                ) :
+                                    question.type == "mcq" ? (
+                                        question?.questionOptions?.map((option) => (
+                                            <label htmlFor="selectedOptionIds" className='mt- flex flex-col gap-1 w-[90%] md:w-1/2' key={option.optionId}>
+                                                <Field type="checkbox" name="selectedOptionIds" id="selectedOptionIds" value={option.optionId} hidden />
+                                                <div className={`py-[18px] text-center text-2xl rounded-4xl border-2 cursor-pointer transition-all duration-500 ease-in-out 
+                                                 ${values.selectedOptionIds?.includes(option.optionId)
+                                                        ? 'border-white bg-[#79D7BE] text-white shadow-[0px_2px_6px_rgba(19,18,66,0.07)]'
+                                                        : 'border-[#152A4C] bg-white'
+                                                    }`}
 
-        </div>
+                                                    onClick={() => {
+                                                        const current = values.selectedOptionIds || []
+                                                        if (current.includes(option.optionId)) {
+                                                            setFieldValue('selectedOptionIds',
+                                                                current.filter(id => id !== option.optionId)
+                                                            )
+                                                        } else {
+                                                            setFieldValue('selectedOptionIds', [...current, option.optionId])
+                                                        }
+                                                    }}
+                                                >
+                                                    {option.text}
+                                                </div>
+                                            </label>
+                                        ))
+                                    ) : null}
+                                <button type='submit' className=' bg-[#152A4C] text-white px-16 py-4 rounded-4xl font-bold self-end transform transition duration-300 hover:scale-105 hover:opacity-90 ease-in-out'
+                                    onClick={() => setFieldValue('questionId', question.questionId)}
+                                >Next</button>
+                            </div>
+                        ))}
+                    </Form>
+                )}
+            </Formik>
+
+
+
+        </div >
     )
 }
--- src/Components/Interview/Prefrences.jsx
+++ src/Components/Interview/Prefrences.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import submit from '../../assets/submit.png'
 import styles from './Interview.module.css'
 import { Link } from 'react-router-dom'
+import { Field, Formik, Form, useFormik } from 'formik'
 
 export default function Prefrences() {
 
@@ -22,73 +23,146 @@ export default function Prefrences() {
         <div className='bg-[#152A4C] '>
             <div className="flex flex-col justify-center items-center min-h-[calc(100vh-68px)]">
                 {/* stepper */}
-                <div className="flex flex-col mx-10 py-8">
-                    <div className="px-10 rounded-4xl h-full sm:h-auto w-200px sm:w-[600px] md:w-[700px] bg-white lg:w-[700px]">
-                        <div className="flex justify-between pt-10 pb-8 border-b border-[#D9DBE9] relative">
-                            {[1, 2, 3, 4].map((num, index) => (
-                                <div className={`w-full h-2 grid text-xl ${styles.stepper} ${step >= num ? "bg-[#79D7BE] text-white" : "bg-[#EFF0F6] text-[#6F6C90]"}`} >
+                <Formik
+                    initialValues={{
+                        experienceLevel: 0,
+                        role: "",
+                        timeLimitInMinutes: 120,
+                        numberOfQuestions: 10,
+                        userId: ""
+                    }}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                    validateOnChange={false}
+                    validateOnBlur={false}
+                >
+                    {({ values, setFieldValue }) => (
+                        <Form >
+                            <div className="flex flex-col mx-10 py-8">
+                                <div className="px-10 rounded-4xl h-full sm:h-auto w-200px sm:w-[600px] md:w-[700px] bg-white lg:w-[700px]">
+                                    <div className="flex justify-between pt-10 pb-8 border-b border-[#D9DBE9] relative">
+                                        {[1, 2, 3, 4].map((num, index) => (
+                                            <div key={index} className={`w-full h-2 grid text-xl ${styles.stepper} ${step >= num ? "bg-[#79D7BE] text-white" : "bg-[#EFF0F6] text-[#6F6C90]"}`} >
+                                            </div>
+                                        ))}
+                                    </div>
+
+                                    <div className='flex flex-col justify-center items-center pt-8 pb-10'>
+                                        {step == 1 ? (
+                                            <>
+                                                <h1 className='text-center text-[#170F49] text-2xl font-bold'>
+                                                    What field do you want to practice for?
+                                                </h1>
+
+                                                <div className="flex flex-col gap-6 mt-8 w-full">
+                                                    {[
+                                                        "Frontend Development",
+                                                        "Backend Development",
+                                                        "Mobile Application Development",
+                                                        "AI & Machine Learning"
+                                                    ].map((option) => (
+                                                        <label key={option} htmlFor={option}>
+                                                            <Field
+                                                                type="radio"
+                                                                name="role"
+                                                                value={option}
+                                                                className="hidden"
+                                                                id={option}
+                                                            />
+                                                            <div
+                                                                onClick={() => setFieldValue('role', option)}
+                                                                className={`bg-white border-2 rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg font-medium cursor-pointer outline-0
+                                                        ${values.role === option
+                                                                        ? 'border-[#79D7BE] text-black'
+                                                                        : 'border-[#EFF0F6] text-[rgba(0,0,0,0.5)]'
+                                                                    }`}
+                                                            >
+                                                                {option}
+                                                            </div>
+                                                        </label>
+                                                    ))}
+                                                </div>
+
+                                            </>
+                                        ) : step == 2 ? (
+                                            <>
+                                                <div className="flex flex-col gap-6 mt-8 w-full">
+                                                    <h1 className='text-center text-[#170F49] text-2xl font-bold'>What’s Your Level?                                    </h1>
+                                                    {["Fresh Graduate", "Junior", "Mid-Level", "Senior"].map((option) => (
+                                                        <label htmlFor={option} key={option}>
+                                                            <Field
+                                                                type="radio"
+                                                                name="experienceLevel"
+                                                                value={option}
+                                                                className="hidden"
+                                                                id={option}
+                                                            />
+                                                            <div onClick={() => setFieldValue("experienceLevel", option)}
+                                                                className={`bg-white border-2 rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg font-medium cursor-pointer outline-0
+                                                            ${values.experienceLevel === option ? 'border-[#79D7BE] text-black' : 'border-[#EFF0F6] text-[rgba(0,0,0,0.5)]'}
+                                                            `}
+                                                            >
+                                                                {option}
+                                                            </div>
+                                                        </label>
+                                                    ))
+                                                    }
+                                                </div>
+                                            </>
+                                        ) : step == 3 ? (
+                                            <>
+                                                <div className="flex flex-col gap-6 mt-8 w-full">
+                                                    <h1 className='text-center text-[#170F49] text-2xl font-bold'>What’s Your Level?                                    </h1>
+                                                    {["1-2 hours", "3-4 hours", "5-8 hours", "8+ hours"].map((option) => (
+                                                        <label htmlFor={option} key={option}>
+                                                            <Field
+                                                                type="radio"
+                                                                name="experienceLevel"
+                                                                value={option}
+                                                                className="hidden"
+                                                                id={option}
+                                                            />
+                                                            <div onClick={() => setFieldValue("experienceLevel", option)}
+                                                                className={`bg-white border-2 rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg font-medium cursor-pointer outline-0
+                                                            ${values.experienceLevel === option ? 'border-[#79D7BE] text-black' : 'border-[#EFF0F6] text-[rgba(0,0,0,0.5)]'}
+                                                            `}
+                                                            >
+                                                                {option}
+                                                            </div>
+                                                        </label>
+                                                    ))
+                                                    }
+                                                </div>
+
+                                            </>
+                                        ) : step == 4 ? (<>
+                                            <div className='flex flex-col justify-center items-center gap-6'>
+                                                <h1 className='text-center text-[#170F49] text-2xl font-bold'>Submit your preferences  </h1>
+                                                <img src={submit} className='w-[150px]' />
+                                                <p className='text-center'>Please review all the information you previously chose in the past steps, and if all is okay, submit your preferences</p>
+                                                <Link to="/WelcomePage">
+                                                    <button type='submit' className='bg-[#79D7BE] text-white px-10 py-3 rounded-[56px] shadow-[0px_3px_12px_rgba(19,18,66,0.07)]'
+                                                        onClick={() => console.log("Submit clicked")}
+                                                    >Submit</button>
+                                                </Link>
+                                            </div>
+                                        </>) : null}
+                                    </div>
                                 </div>
-                            ))}
-                        </div>
-                        <div className='flex flex-col justify-center items-center pt-8 pb-10'>
-                            {step == 1 && (
-                                <>
-                                    <h1 className='text-center text-[#170F49] text-2xl font-bold'>What field do you want to practice for? </h1>
-                                    <div className="flex flex-col gap-6 mt-8 w-full">
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='Frontend Development' readOnly />
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='Backend Development' readOnly />
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='Mobile Application Development' readOnly />
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='AI & Machine Learning' readOnly />
-                                    </div>
-                                </>
-                            )}
-                            {step == 2 && (
-                                <>
-                                    <h1 className='text-center text-[#170F49] text-2xl font-bold'>What’s Your Level?                                    </h1>
-                                    <div className="flex flex-col gap-6 mt-8 w-full">
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='Fresh Graduate' readOnly />
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='Junior' readOnly />
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='Mid-Level' readOnly />
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='Senior' readOnly />
-                                    </div>
-                                </>
-                            )}
-                            {step == 3 && (
-                                <>
-                                    <h1 className='text-center text-[#170F49] text-2xl font-bold'>How many hours do you plan to study each day?</h1>
-                                    <div className="flex flex-col gap-6 mt-8 w-full">
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='1-2 hours' readOnly />
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='3-4 hours' readOnly />
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='5-8 hours' readOnly />
-                                        <input className="bg-white border-2 border-[#EFF0F6] rounded-4xl shadow-[0px_2px_6px_rgba(19,18,66,0.07)] px-8 py-4 text-lg text-[rgb(0,0,0,0.5)] font-medium cursor-pointer focus:border-[#79D7BE] outline-0 focus:text-black" placeholder='8+ hours' readOnly />
-                                    </div>
-                                </>
-                            )}
-                            {step == 4 && (
-                                <>
-                                    <div className='flex flex-col justify-center items-center gap-6'>
-                                        <h1 className='text-center text-[#170F49] text-2xl font-bold'>Submit your preferences  </h1>
-                                        <img src={submit} className='w-[150px]' />
-                                        <p className='text-center'>Please review all the information you previously chose in the past steps, and if all is okay, submit your preferences</p>
-                                        <Link to="/WelcomePage">
-                                            <button className='bg-[#79D7BE] text-white px-10 py-3 rounded-[56px] shadow-[0px_3px_12px_rgba(19,18,66,0.07)]'>Submit</button>
-                                        </Link>
-                                    </div>
-                                </>
-                            )}
+                                <div className="flex justify-between items-center w-full mt-8 ">
+                                    <button type='button' className={`border border-[#79D7BE] text-[#79D7BE] px-10 py-3 rounded-[56px] ${step == 1 ? "hidden" : "block "}`} onClick={prevStep}>previous</button>
+                                    <button type='button' className={`bg-[#79D7BE] text-white px-10 py-3 rounded-[56px]  self-end ${step == 4 ? "hidden" : "ml-auto"} `} onClick={nextStep}>Next step</button>
+                                </div>
+                            </div>
+                        </Form>
+                    )
+                    }
+                </Formik >
 
-                        </div>
-                    </div>
-                    <div className="flex justify-between items-center w-full mt-8 ">
-                        <button className={`border border-[#79D7BE] text-[#79D7BE] px-10 py-3 rounded-[56px] ${step == 1 ? "hidden" : "block "}`} onClick={prevStep}>previous</button>
-                        <button className={`bg-[#79D7BE] text-white px-10 py-3 rounded-[56px]  self-end ${step == 4 ? "hidden" : "ml-auto"} `} onClick={nextStep}>Next step</button>
-                    </div>
-                </div>
-
-
-
-            </div>
+            </div >
         </div >
+
     )
 }
 // {/* <div className="border-b-2 border-[#D9DBE9] pb-8 "> */ }
